93f419d344a37cdecdfaec791b0e41542d7df1c1
Janil Caires Garcia Junior <janilcgarcia@gmail.com>
2017-05-02 20:23:07 -0400

    Added GDX

---
+++ desktop/src/br/ufms/mechsmasher/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package br.ufms.mechsmasher.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import br.ufms.mechsmasher.MechSmasher;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MechSmasher(), config);
+	}
+}
