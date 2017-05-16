38a6cd7d05f5e4ef34066159927d1d683ecb36af
Janil Caires Garcia Junior <janilcgarcia@gmail.com>
2017-05-16 16:32:15 -0400

    Gameplay básico

--- desktop/src/br/ufms/mechsmasher/desktop/DesktopLauncher.java
+++ desktop/src/br/ufms/mechsmasher/desktop/DesktopLauncher.java
@@ -7,6 +7,10 @@ import br.ufms.mechsmasher.MechSmasher;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		// O tamanho da tela é fixo e widescreen (16:9)
+		config.width = 960;
+		config.height = 540;
 		new LwjglApplication(new MechSmasher(), config);
 	}
 }
